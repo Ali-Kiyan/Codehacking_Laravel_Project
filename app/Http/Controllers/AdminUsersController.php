ecde906c3791c91fbcaa54f440f6c9435540a723
Ali-Kiyan <Alikiyand@gmail.com>
2017-08-01 17:27:07 +0100

    adding creating users with validations

--- app/Http/Controllers/AdminUsersController.php
+++ app/Http/Controllers/AdminUsersController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\UsersRequest;
+use App\Role;
+use App\User;
 use Illuminate\Http\Request;
 
 use App\Http\Requests;
@@ -16,7 +19,8 @@ class AdminUsersController extends Controller
     public function index()
     {
         //
-        return view('admin.users.index');
+        $users = User::all();
+        return view('admin.users.index', compact('users'));
     }
 
     /**
@@ -28,7 +32,8 @@ class AdminUsersController extends Controller
     {
         //
 
-        return view('admin.users.create');
+        $roles = Role::lists('name','id')->all(); //order is matter here
+        return view('admin.users.create', compact('roles'));
     }
 
     /**
@@ -37,9 +42,13 @@ class AdminUsersController extends Controller
      * @param  \Illuminate\Http\Request  $request
      * @return \Illuminate\Http\Response
      */
-    public function store(Request $request)
+    public function store(UsersRequest $request)
     {
-        //
+        //persisiting data
+        User::create($request->all());
+        return redirect('/admin/users');
+//        return $request->all();
+
     }
 
     /**

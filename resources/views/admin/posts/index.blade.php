fcd3730c3c1befc22102e7e23e0329f3c07bf703
Ali-Kiyan <Alikiyand@gmail.com>
2017-08-02 17:16:10 +0100

    adding dynamic categories

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -34,7 +34,7 @@
                     <td>{{$post->id}}</td>
                     <td><img src="{{$post->photo ? $post->photo->file: '/images/unknown.png'}}" alt="" style="border-radius: 10%;height: 20px;"></td>
                     <td>{{$post->user->name}}</td>
-                    <td>{{$post->category_id}}</td>
+                    <td>{{$post->category? $post->category->name:'Uncategorized'}}</td>
                     <td>{{$post->title}}</td>
                     <td>{{$post->body}}</td>
                     <td>{{$post->created_at->diffForhumans()}}</td>
